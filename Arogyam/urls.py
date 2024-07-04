b80ccc6ff7b3ae229466943b88dbb1fc2b056042
Gourav Lohar <gouravlohar640@gmail.com>
2024-07-05 00:31:09 +0530

    index.html

--- Arogyam/urls.py
+++ Arogyam/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import index
+from .views import index,heart_predict
 
 urlpatterns = [
-    path("", index),
+    path("", index,name="home"),
+    path("heart_predict/", heart_predict,name="heart_predict"),
 ]
--- Arogyam/views.py
+++ Arogyam/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse
-
 # Create your views here.
 def index (request):
     return render(request,'index.html')
+def heart_predict(request):
+    return render(request,'heart_predict.html')
